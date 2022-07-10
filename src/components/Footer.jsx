880489f5f994dc49e9d0edf7f1fad203ca5ff9c7
Samer A <thedev.samer@gmail.com>
2022-07-10 10:01:46 +0300

    finished building the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
+import Logo from "./common/Logo";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <Logo link={true} linkTo={"/"} width="70px" height="70px" />
+      </div>
+      <div className="footer_descl">
+        Manchester City 2022. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
