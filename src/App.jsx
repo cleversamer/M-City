664c852997991bcfa07779fbe5e2c55784455c81
Samer A <thedev.samer@gmail.com>
2022-07-10 09:53:24 +0300

    finished building the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { Fragment } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/not-found" element={<NotFound />} />
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Navigate to="/not-found" replace />} />
-    </Routes>
+    <Fragment>
+      <Header />
+
+      <Routes>
+        <Route path="/not-found" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+      </Routes>
+
+      <Footer />
+    </Fragment>
   );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Button } from "@mui/material";
+
+const Header = () => {
+  const style = {
+    backgroundColor: "#98c5e9",
+    boxShadow: "none",
+    paddin: "10px 0",
+    borderBottom: "2px solid #00285e",
+  };
+
+  return (
+    <AppBar position="fixed" style={style}>
+      <Toolbar style={{ display: "flex" }}>
+        <div style={{ flexGrow: 1 }}>
+          <div className="header_logo">logo</div>
+        </div>
+
+        <Link to="/team">
+          <Button color="inherit">The team</Button>
+        </Link>
+
+        <Link to="/matches">
+          <Button color="inherit">Matches</Button>
+        </Link>
+
+        <Link to="/dashboard">
+          <Button color="inherit">Dashboard</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
