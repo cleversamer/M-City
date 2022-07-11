63aab4ccbcec75a85a8627b924fdce29af95ceb5
Samer A <thedev.samer@gmail.com>
2022-07-11 09:35:34 +0300

    implemented logout feature with firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,33 @@
 import { Link } from "react-router-dom";
+
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, logoutUser } from "../store/user";
+
 import { AppBar, Toolbar, Button } from "@mui/material";
 import Logo from "./common/Logo";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
   const style = {
     backgroundColor: "#98c5e9",
     boxShadow: "none",
     paddin: "10px 0",
     borderBottom: "2px solid #00285e",
+  };
+
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(logoutUser());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -27,9 +47,17 @@ const Header = () => {
           <Button color="inherit">Matches</Button>
         </Link>
 
-        <Link to="/dashboard">
-          <Button color="inherit">Dashboard</Button>
-        </Link>
+        {user && (
+          <>
+            <Link to="/dashboard">
+              <Button color="inherit">Dashboard</Button>
+            </Link>
+
+            <Button color="inherit" onClick={handleSignout}>
+              Logout
+            </Button>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
