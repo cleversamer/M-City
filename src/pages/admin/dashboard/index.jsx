4432b497c74844d557f68321f5ac9b09c74f25a3
Samer A <thedev.samer@gmail.com>
2022-07-12 11:27:48 +0300

    fixed a styling issue in the admin layout hoc

--- src/pages/admin/dashboard/index.jsx
+++ src/pages/admin/dashboard/index.jsx
@@ -2,7 +2,7 @@ import AdminLayout from "../../../hoc/AdminLayout";
 
 const Dashboard = () => {
   return (
-    <AdminLayout>
+    <AdminLayout title="Dashboard">
       <div className="user_dashboard">
         <div>This is your dashboard</div>
       </div>
--- src/pages/admin/players/index.jsx
+++ src/pages/admin/players/index.jsx
@@ -1,5 +1,11 @@
+import AdminLayout from "../../../hoc/AdminLayout";
+
 const AdminPlayers = () => {
-  return <div> AdminPlayers</div>;
+  return (
+    <AdminLayout title="The players">
+      <div></div>
+    </AdminLayout>
+  );
 };
 
 export default AdminPlayers;
